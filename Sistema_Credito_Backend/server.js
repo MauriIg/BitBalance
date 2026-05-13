d3639beba7a38533390d83e76cee6a5e1f0cd1ac
Mauricio Damian Gomez Galdamez <L21270287@tuxtla.tecnm.mx>
2026-05-12 23:41:01 -0600

    Fix reinversiones route

--- Sistema_Credito_Backend/server.js
+++ Sistema_Credito_Backend/server.js
@@ -45,7 +45,7 @@ app.use("/api/restock", restockRoutes);
 app.use("/api/stock", stockRoutes);
 app.use("/api/pedidos-proveedor", supplierOrderRoutes);
 app.use("/api/categorias", categoriaRoutes);
-app.use("/api/reinversionesRoutes", reinversionRoutes);
+app.use("/api/reinversiones", reinversionRoutes);
 
 // Ruta de prueba
 app.get("/", (req, res) => res.send("🚀 API corriendo correctamente"));
